38b88e9c66dd9af5f73462e7253e315560707835
novendrasatria <novendradotid@gmail.com>
2019-04-10 19:36:23 +0700

    Upload Tugas 5.

---
+++ main.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table,tr,td{
+background-color: LightGoldenRodYellow ;
+font-family: century gothic;
+color: green;
+border: 1px solid aqua;
+padding: 5px;
+border-collapse: collapse;
+}
+.h{
+border: 8px groove yellow;
+padding: 20px;
+}
+.posisi{
+position: absolute;
+margin-left: auto;
+margin-right: auto;
+margin-bottom: auto;
+margin-top: auto;
+left: 0;
+right: 0;
+top: 100px;
+bottom: 0;
+}
+body{
+background-repeat: no-repeat;
+background-size: 1400px auto;
+}
+</style>
+<meta name="description" content="Biodata"/>
+<meta name="Keywords" content="Biodata"/>
+<meta name="authors" content="Angga"/>
+<meta charset="UTF-8"/>
+<title>Biodata</title>
+</head>
+<body>
+<form action="#" style="width: 1000px"class="posisi";>
+<fieldset class="h"/>
+
+<table style="width: 980px;">
+<tr>
+<td rowspan="15" width="250px">
+<img src="mancing.jpg" width="250px" height="420px"/>
+</tr>
+<tr>
+<td><b>Nama Lengkap</b></td>
+<td>:</td>
+<td>Novendra Satria Putra</td>
+</tr>
+<tr>
+<td><b>Nama Panggilan</b></td>
+<td>:</td>
+<td>Novendra</td>
+</tr>
+<tr>
+<td><b>Tempat, Tanggal Lahir</b></td>
+<td>:</td>
+<td>Sleman, 19 November 2000</td>
+</tr>
+<tr>
+<td><b>Umur</b></td>
+<td>:</td>
+<td>18 Tahun</td>
+</tr>
+<tr>
+<td><b>Jenis Kelamin</b></td>
+<td>:</td>
+<td>Laki - Laki</td>
+</tr>
+<tr>
+<td><b>Perguruan Tinggi</b></td>
+<td>:</td>
+<td>STMIK AKAKOM</td>
+</tr>
+<tr>
+<td><b>Agama</b></td>
+<td>:</td>
+<td>Islam</td>
+</tr>
+<tr>
+<td><b>Alamat</b></td>
+<td>:</td>
+<td>Gamping, Sleman</td>
+</tr>
+<tr>
+<td><b>Status</b></td>
+<td>:</td>
+<td>Belum Menikah</td>
+</tr>
+<tr>
+<td><b>Pekerjaan</b></td>
+<td>:</td>
+<td>Mahasiswa</td>
+</tr>
+<tr>
+<td><b>Kewarganegaraan</b></td>
+<td>:</td>
+<td>Indonesia</td>
+</tr>
+<tr>
+<td><b>NIM</b></td>
+<td>:</td>
+<td>175610028</td>
+<td colspan="1" align="left">
+</td>
+</tr>
+</table>
+</fieldset>
+</form>
+<?php
+  echo "";
+?>
+</body>
+</html>
